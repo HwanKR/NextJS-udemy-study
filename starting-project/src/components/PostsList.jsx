52b652ea8fe0c8170c693255fc08175feb17e611
HwanKR <fabronjeon@naver.com>
2025-08-24 10:08:02 +0900

    Section 2-28 complete

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,14 +1,29 @@
-// import Post from './Post';
+import { useState } from 'react';
+
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function onAuthorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={onAuthorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="hwan" body="test1" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="rue" body="test2" />
       </ul>
     </>
