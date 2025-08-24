ba70e762e20fb30bee9543ac836d4edcf8041e1a
HwanKR <fabronjeon@naver.com>
2025-08-24 09:37:03 +0900

    Section 2-25 complete

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,10 +1,12 @@
 // import Post from './Post';
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostsList() {
   return (
     <>
+      <NewPost />
       <ul className={classes.posts}>
         <Post author="hwan" body="test1" />
         <Post author="rue" body="test2" />
